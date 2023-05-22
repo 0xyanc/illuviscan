2ec9afd1b6eaa1533a164bf2bd7c8a56ed7cba00
0xyanc <yannick.chi@gmail.com>
2023-05-22 17:13:01 +0200

    Add footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,5 +1,11 @@
+import { Flex, Text } from "@chakra-ui/react";
+
 const Footer = () => {
-  return <></>;
+  return (
+    <Flex justifyContent="center">
+      <Text>Made by 0xyanc.eth</Text>
+    </Flex>
+  );
 };
 
 export default Footer;
